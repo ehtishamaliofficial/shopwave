4a677d4fd89680c8bceb8cc2cacf50c04a400130
Ehtisham Ali <ehtisham.rajpoot.official@gmail.com>
2026-05-04 11:56:44 +0500

    add swagger in the user service

---
+++ services/user-service/src/main/java/com/shami/userservice/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.shami.userservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI userServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("User Service API")
+                        .description("ShopWave user management and address APIs")
+                        .version("1.0.0"));
+    }
+}
